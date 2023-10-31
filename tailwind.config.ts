dc0028751663c2a356f449dea6f2125bcf337de5
Hasib Hossain <boraborhasib@gmail.com>
2023-10-31 22:52:06 +0600

    Updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors: {
+        primary: '#F875AA',
+        secondary: '#FFDFDF',
+        tertiary: '#FFF6F6',
+        'light-blue': '#AEDEFC',
+      }
     },
   },
   plugins: [],
