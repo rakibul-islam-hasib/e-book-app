6b1e24475b75b92fada5b2f6faa3b78df7355ec6
Hasib Hossain <boraborhasib@gmail.com>
2023-10-31 23:10:38 +0600

    Some navbar update changed

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,55 @@
+import Link from 'next/link';
 import React, { FC } from 'react';
 
+type NavLinks = {
+    id: number;
+    name: string;
+    path: string;
+}
+
 const NavBar: FC = () => {
+    const navLinks: NavLinks[] = [
+        {
+            id: 1,
+            name: 'Home',
+            path: '/'
+        },
+        {
+            id: 2,
+            name: 'About',
+            path: '/about'
+        },
+        {
+            id: 3,
+            name: 'Contact',
+            path: '/contact'
+        }
+    ]
+
+
     return (
         <nav>
-            
+            <div className="max-w-screen-xl flex items-center justify-between py-3 mx-auto">
+                <div className="logo">
+                    <h1>Ebuker</h1>
+                </div>
+                <div className="search">
+                    <input type="text" className='outline-none' spellCheck={false} placeholder="Search" />
+                </div>
+                <div className="links">
+                    <ul className='flex items-center space-x-4 text-lg'>
+                        {
+                            navLinks.map((link) => {
+                                return (
+                                    <li key={link.id}>
+                                        <Link href={link.path}>{link.name}</Link>
+                                    </li>
+                                );
+                            })
+                        }
+                    </ul>
+                </div>
+            </div>
         </nav>
     );
 };
