d0b65004752d7063ff1cacfce80248d785089bf3
Hasib Hossain <boraborhasib@gmail.com>
2023-11-02 01:04:00 +0600

    updated  : Hero

--- src/app/(root-layout)/home/Hero.tsx
+++ src/app/(root-layout)/home/Hero.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import '@/style/Hero.css'
 
 const Hero: React.FC = () => {
     return (
         <div className='h-screen w-full flex justify-center items-center'>
             <div className="">
-                <h1 className='font-Bebas_Neue text-5xl text-light-blue'>Coming soon</h1>
+                <div className="container">
+                    <div className="content">
+                        <div className="content__container">
+                            <p className="content__container__text">
+                                Hello
+                            </p>
+
+                            <ul className="content__container__list">
+                                <li className="content__container__list__item">world !</li>
+                                <li className="content__container__list__item">coder !</li>
+                                <li className="content__container__list__item">users !</li>
+                                <li className="content__container__list__item">uiverse</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
