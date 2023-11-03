68ab02dfd4d16a202eba52411159169ec6fca13c
Hasib Hossain <boraborhasib@gmail.com>
2023-11-03 12:12:48 +0600

    Tabs added

--- src/app/(root-layout)/home/components/TabsForTopBooks.tsx
+++ src/app/(root-layout)/home/components/TabsForTopBooks.tsx
@@ -1,10 +1,36 @@
-import React, { FC } from 'react';
+'use client';
+import { Card, CardBody, Tab, Tabs } from '@nextui-org/react';
+import React, { FC, useState } from 'react';
 
 const TabsForTopBooks: FC = () => {
+    const [active, setActive] = useState('music');
     return (
-        <div>
+        <section>
+            <div className="flex flex-col justify-center items-center">
+                <Tabs selectedKey={active} onSelectionChange={setActive} aria-label="Options" color='primary'>
 
-        </div>
+                    <Tab key="photos" title="Photos">
+                        <div className="">
+                            <h1>Bols </h1>
+                        </div>
+                    </Tab>
+                    <Tab key="music" title="Music">
+                        <Card>
+                            <CardBody>
+                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            </CardBody>
+                        </Card>
+                    </Tab>
+                    <Tab key="videos" title="Videos">
+                        <Card>
+                            <CardBody>
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </CardBody>
+                        </Card>
+                    </Tab>
+                </Tabs>
+            </div>
+        </section>
     );
 };
 
