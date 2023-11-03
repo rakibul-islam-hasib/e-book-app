ddd38f77bed772d5de9aab580b484859c88a7212
Hasib Hossain <boraborhasib@gmail.com>
2023-11-03 12:20:48 +0600

    Error fixed

--- src/app/(root-layout)/home/components/TabsForTopBooks.tsx
+++ src/app/(root-layout)/home/components/TabsForTopBooks.tsx
@@ -3,7 +3,7 @@ import { Card, CardBody, Tab, Tabs } from '@nextui-org/react';
 import React, { FC, useState } from 'react';
 
 const TabsForTopBooks: FC = () => {
-    const [active, setActive] = useState('music');
+    const [active, setActive] = useState<any>('music');
     return (
         <section>
             <div className="flex flex-col justify-center items-center">
